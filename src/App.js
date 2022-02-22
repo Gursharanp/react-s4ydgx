47d18089a62e27b86c6166ccc32ff1908984259c
Gursharanp <gursharanpreetsinghkang2@gmail.com>
2022-02-22 12:19:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React,{useState} from "react";
 import "./style.css";
 import Component from './Component.js'
 export default function App() {
-  const[flag,setFlag]=useState(true);
+  const[show,setShow]=useState(true);
   return (
     <>
-      <button onClick={()=>setFlag(!flag)}>Switch</button>
-      {flag && <Component/>}
+      <button onClick={()=>setFlag(!show)}>Switch</button>
+      {show && <Component/>}
     </>
   );
 }
